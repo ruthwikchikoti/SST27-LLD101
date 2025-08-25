95629b6f1f640129e0aa821348785116f96ce178
ruthwikchikoti <chikotiruthwik@gmail.com>
2025-08-25 11:45:32 +0530

    ex10 refactored to follow SOLID principles

--- SOLID/ex10/src/ReportService.java
+++ SOLID/ex10/src/ReportService.java
@@ -1,6 +1,10 @@
 public class ReportService {
+    Logger logger;
+
+    public ReportService(Logger logger){
+        this.logger = logger;
+    }
     void generate(){
-        ConsoleLogger logger = new ConsoleLogger();
         logger.log("Generating daily report...");
         System.out.println("Report contents...");
     }
